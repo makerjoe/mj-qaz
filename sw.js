0d8e575c849a70b9aa48d14cfd580fa086fe7a0b
makerjoe <makerjoe@gmail.com>
2020-11-15 20:55:49 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -4,11 +4,13 @@ var filesToCache = [
   '/qaz/index.html',
   '/qaz/css/style.css',
   '/qaz/js/main.js',
-  '/qaz/js/app.js'
+  '/qaz/js/app.js',
+  '/qaz/pages/about.html
 ];
 
 /* Start the service worker and cache all of the app's content */
 self.addEventListener('install', function(e) {
+  console.log('Start the service worker and cache all of the app's content')
   e.waitUntil(
     caches.open(cacheName).then(function(cache) {
       return cache.addAll(filesToCache);
@@ -18,6 +20,7 @@ self.addEventListener('install', function(e) {
 
 /* Serve cached content when offline */
 self.addEventListener('fetch', function(e) {
+  console.log('Serve cached content when offline')
   e.respondWith(
     caches.match(e.request).then(function(response) {
       return response || fetch(e.request);
