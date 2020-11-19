b9fd07eebc905f5aa11d1df7af0bccf1f18ccb23
makerjoe <makerjoe@gmail.com>
2020-11-19 03:17:14 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,6 +2,18 @@ window.onload = () => {
   'use strict';
 
   if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('./service-worker.js',{ scope: '.' });
-  }
+    navigator.serviceWorker.register('/qaz/service-worker.js', { scope: '/qaz/' }).then(function(reg) {
+
+    if(reg.installing) {
+      console.log('Service worker installing');
+    } else if(reg.waiting) {
+      console.log('Service worker installed');
+    } else if(reg.active) {
+      console.log('Service worker active');
+    }
+
+  }).catch(function(error) {
+    // registration failed
+    console.log('Registration failed with ' + error);
+  });
 }
