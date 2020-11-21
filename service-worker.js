06927168d7819c35e6b3473ee5b9deb644fbb675
makerjoe <makerjoe@gmail.com>
2020-11-21 18:33:29 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,13 +1,13 @@
 var cacheName = 'qaz-pwa';
 var filesToCache = [
-  '/',
-  '/index.html',
-  '/css/style.css',
-  '/js/main.js',
-  '/pages/about.html',
-  '/pages/form.html',
-  '/framework7/js/framework7.bundle.js',
-  '/framework7/css/framework7.bundle.css'
+  '/qaz/',
+  '/qaz/index.html',
+  '/qaz/css/style.css',
+  '/qaz/js/main.js',
+  '/qaz/pages/about.html',
+  '/qaz/pages/form.html',
+  '/qaz/framework7/js/framework7.bundle.js',
+  '/qaz/framework7/css/framework7.bundle.css'
 ];
 
 /* Start the service worker and cache all of the apps content */
